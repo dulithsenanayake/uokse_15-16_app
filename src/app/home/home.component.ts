35f4c6430b0077ca3a8f3d09f9a96144bfb34f28
dulithsenanyake <dulithcsenanayake@gmail.com>
2020-08-09 14:49:52 +0530

    set file upload from UI

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import { AdalService } from 'adal-angular4';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpErrorResponse, HttpEventType} from '@angular/common/http';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatTableDataSource} from '@angular/material/table';
+import {HomeService} from './home.service';
+import {catchError, map} from 'rxjs/operators';
+import {of} from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -13,10 +16,11 @@ export class HomeComponent implements OnInit {
 
   user: any;
   profile: any;
-  displayedColumns: string[] = ['id','stu_no','name','fileupload'];
+  displayedColumns: string[] = ['id','stu_no','name'];
   data = [];
-  constructor(private adalService: AdalService, protected http: HttpClient) { }
-
+  @ViewChild("fileUpload") fileUpload: ElementRef;
+  files  = [];
+  constructor(private adalService: AdalService, protected http: HttpClient,protected homeService:HomeService) { }
   dataSource = new MatTableDataSource<PeriodicElement>();
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
@@ -45,18 +49,47 @@ export class HomeComponent implements OnInit {
       }
     });
   }
-  public getProfile() {
-    console.log('Get Profile called');
-    return this.http.get("https://graph.microsoft.com/v1.0/me");
-  }
 
-  public profileClicked() {
-    this.getProfile().subscribe({
-      next: result => {
-        console.log('Profile Response Received');
-        this.profile = result;
+  uploadFile(file) {
+    const formData = new FormData();
+    formData.append('file', file.data);
+    file.inProgress = true;
+    this.homeService.upload(formData).pipe(
+      map(event => {
+        switch (event.type) {
+          case HttpEventType.UploadProgress:
+            file.progress = Math.round(event.loaded * 100 / event.total);
+            break;
+          case HttpEventType.Response:
+            return event;
+        }
+      }),
+      catchError((error: HttpErrorResponse) => {
+        file.inProgress = false;
+        return of(`${file.data.name} upload failed.`);
+      })).subscribe((event: any) => {
+      if (typeof (event) === 'object') {
+        console.log(event.body);
       }
     });
+  }
+  private uploadFiles() {
+    this.fileUpload.nativeElement.value = '';
+    this.files.forEach(file => {
+      this.uploadFile(file);
+    });
+  }
+  onClick() {
+    const fileUpload = this.fileUpload.nativeElement;
+    fileUpload.onchange = () => {
+      for (let index = 0; index < fileUpload.files.length; index++)
+      {
+        const file = fileUpload.files[index];
+        this.files.push({ data: file, inProgress: false, progress: 0});
+      }
+      this.uploadFiles();
+    };
+    fileUpload.click();
   }
 }
 
